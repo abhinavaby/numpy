02572ec999f7e8a6e705632e3545c84066bf0a7b
abhinavaby <abhinavaby07@gmail.com>
2026-02-06 21:32:03 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,9 @@
+import numpy as np
+numpy_array=np.array([1, 2, 3, 4, 5])
+print(numpy_array)
+print()
+array_2d=np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(array_2d)
+print()
+array_3d=np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print(array_3d)
