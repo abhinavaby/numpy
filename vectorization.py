1c6c090fcd561dc3522847fcb81e64fbbafc746c
abhinavaby <abhinavaby07@gmail.com>
2026-02-08 20:16:34 +0530

    Add files via upload

---
+++ vectorization.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr=np.array([1, 2, 3, 4, 5])
+arr2=np.array([10, 20, 30, 40, 50])
+result=arr+arr2
+print("Result of element-wise addition:", result)
+result2=arr*arr2
+print("Result of element-wise multiplication:", result2)
+print()
