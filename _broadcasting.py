ecec7e62cb77a2b0cebc3a30f8c58232c35087c5
abhinavaby <abhinavaby07@gmail.com>
2026-02-08 20:14:46 +0530

    Add files via upload

---
+++ _broadcasting.py
@@ -0,0 +1,22 @@
+import numpy as np
+arr=np.array([10, 20, 30, 40, 50])
+discount=10
+# Broadcasting the discount to each element in the array
+finalprice=arr-(arr*discount/100)
+print("Original prices:", arr)
+print("Final prices after applying discount:", finalprice)  
+print()
+arr2=np.array([1,2,3,45,5])
+result=arr*2
+print("Original array:", arr)
+print("Result after broadcasting multiplication:", result)
+print()
+arr2d=np.array([[1,2,3],[4,5,6],[7,8,9]])
+vector=np.array([1,2,3])
+result2d=arr2d+vector
+print("Original 2D array:")
+print(arr2d)
+print("Vector to be added:")
+print(vector)
+print("Result after broadcasting addition:")
+print(result2d) 
