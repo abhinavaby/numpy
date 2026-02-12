e204d9fc50c9724cb62cf389f85f02d5a6418977
abhinavaby <abhinavaby07@gmail.com>
2026-02-12 23:14:41 +0530

    Update _broadcasting.py

--- _broadcasting.py
+++ _broadcasting.py
@@ -1,6 +1,6 @@
 import numpy as np
 arr=np.array([10, 20, 30, 40, 50])
-discount=10
+discount
 # Broadcast the disco to each element in the 
 finalprice=arr-(arr*discount/100)
 print("Original prices:", arr)
