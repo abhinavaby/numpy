ebc962936bca8641ae822e1973484c9584909bb9
abhinavaby <abhinavaby07@gmail.com>
2026-02-25 20:50:28 +0530

    Update _broadcasting.py

--- _broadcasting.py
+++ _broadcasting.py
@@ -4,7 +4,7 @@ discount=10
 # Broadcasting the discount to each element in the array
 finalprice=arr-(arr*discount/100)
 print("Original prices:", arr)
-print("Final prices after applying discount:", finalprice)  
+print("Final prices after applying discount:", 
 print()
 arr2=np.array([1,2,3,45
 result=arr*2
