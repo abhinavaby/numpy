190f2e87468364be1303f0a1b1516cbe9b593d16
abhinavaby <abhinavaby07@gmail.com>
2026-02-25 20:50:52 +0530

    Update _broadcasting.py

--- _broadcasting.py
+++ _broadcasting.py
@@ -4,16 +4,16 @@ discount=10
 # Broadcasting the discount to each element in the array
 finalprice=arr-(arr*discount/100)
 print("Original prices:", arr)
-print("Final prices after applying discount:", 
+print("Final prices after applying discount:", finalprice)  
 print()
-arr2=np.array([1,2,3,45
+arr2=np.array([1,2,3,45,5])
 result=arr*2
 print("Original array:", arr)
 print("Result after broadcasting multiplication:", result)
 print()
 arr2d=np.array([[1,2,3],[4,5,6],[7,8,9]])
-vector=np.array([1,2
-result2d=arr2d+ve
+vector=np.array([1,2,3])
+result2d=arr2d+vector
 print("Original 2D array:")
 print(arr2d)
 print("Vector to be added:")
