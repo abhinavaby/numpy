9f906ab8ec378c8201861ac51cd7b70771478306
abhinavaby <abhinavaby07@gmail.com>
2026-02-28 22:38:14 +0530

    Set discount value for price calculation

--- _broadcasting.py
+++ _broadcasting.py
@@ -1,6 +1,6 @@
 import numpy as np
 arr=np.array([10, 20, 30, 40, 50])
-discount=
+discount=10
 # Broadcasting the discount to each element in the array
 finalprice=arr-(arr*discount/100)
 print("Original prices:", arr)
